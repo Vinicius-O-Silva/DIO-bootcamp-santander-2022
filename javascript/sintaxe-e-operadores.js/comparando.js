67c41cae352b6b2b5b9e64e445ceb80d1a0b02ff
Vinícius Oliveira da Silva <98286138+Vinicius-O-Silva@users.noreply.github.com>
2022-07-30 20:44:06 -0300

    Update comparando.js

--- javascript/sintaxe-e-operadores.js/comparando.js
+++ javascript/sintaxe-e-operadores.js/comparando.js
@@ -17,6 +17,45 @@ function comparacao(num1, num2) {
     return `Os numeros ${num1} e ${num2} ${igual} iguais. Sua soma é ${soma}, que é ${compara10} que 10 e ${compara20} que 20".`
 }
 
-console.log(comparacao(1, 19))
+// Com funções separadas e concatenando Strings
+
+function comparaNumeros (num1, num2) {
+    const frase1 = primeiraFrase(num1, num2);
+    const frase2 = segundaFrase(num1, num2);
+
+    return `${frase1}${frase2}`
+}
+
+function primeiraFrase (num1, num2){
+    let igual = 'não são';
+
+    if(num1 === num2) {
+        igual = 'são'
+    }
+
+    return `Os números ${num1} e ${num2} ${igual} iguais. `
+}
+
+function segundaFrase (num1, num2) {
+    const soma = num1 + num2;
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+
+    if(compara10) {
+        resultado10 = 'maior'
+    }
+
+    if(compara20) {
+        resultado20 = 'maior'
+    }
+    
+    return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20".`
+}
+
+
+console.log(comparacao(1, 2))
+console.log(comparaNumeros(1, 2))
 
 // Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.
