a19ad1fbb85c729ae5f6aca7898f487dc67fe5b2
Vinícius Oliveira da Silva <98286138+Vinicius-O-Silva@users.noreply.github.com>
2022-08-08 17:08:46 -0300

    Strategy

--- desafios-de-projeto-dio/padroes-de-projeto-java/padroes-projeto-java/src/dio/gof/Test.java
+++ desafios-de-projeto-dio/padroes-de-projeto-java/padroes-projeto-java/src/dio/gof/Test.java
@@ -1,8 +1,14 @@
 package dio.gof;
 
+import dio.gof.singleton.SingletonEager;
+import dio.gof.singleton.SingletonLazy;
+import dio.gof.singleton.SingletonLazyHolder;
+import dio.gof.strategy.*;
+
 public class Test {
     public static void main(String[] args) {
-        // Testes relacionados ao Design Patter Singleton
+        // Testes relacionados ao Design Pattern
+        // Singleton
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
@@ -17,5 +23,22 @@ public class Test {
         System.out.println(lazyholder);
         lazyholder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyholder);
+
+        // Strategy
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
     }
 }
